fa291a4bc5b0f3969c206407e918a166a3a295e3
Roman Filatov <roman.filatov@gamingtec.com>
2019-01-25 17:35:30 +0200

    initial commit

---
+++ background.js
@@ -0,0 +1,28 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.contentScriptQuery == 'fetchUrl') {
+
+
+        fetch(request.url, {
+            headers: new Headers({
+                'X-PINGOTHER': 'pingpong',
+                'Content-Type': 'application/xml'
+            })
+        })
+        .then(response => response.text())
+        .then(html => {
+            let parsedText = parseText(html);
+            return sendResponse(parsedText);
+        })
+        .catch(error => console.log(error));
+        return true;
+      }
+    }
+);
+
+function parseText(html) {
+    let from = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->';
+    let to = '<!-- MxM banner -->';
+
+    return html.split(from)[1].split(to)[0].replace(/\<\/div\>/, '');
+}
---
+++ index.js
@@ -0,0 +1,83 @@
+const LyricsLoader = {
+    playerSongInfo: document.getElementById('playerSongInfo'),
+    playerControls: document.getElementsByClassName('material-player-middle'),
+
+    playerElements: {
+        title: 'currently-playing-title',
+        artist: 'player-artist'
+    },
+
+    currentSong: {
+        title: null,
+        artist: null
+    },
+
+    loadTextButton: null,
+    textPopup: null,
+
+    init: function () {
+        this.initObserver();
+        document.addEventListener('click', this.onClick.bind(this), false);
+    },
+
+    initObserver: function() {
+        let observer = new MutationObserver(this.onSongChange.bind(this));
+        observer.observe(this.playerSongInfo, {
+            childList: true
+        });
+    },
+
+    onSongChange: function (mutations, observer) {
+        let songTitleEl = document.getElementById(this.playerElements.title);
+        let songArtistEl = document.getElementById(this.playerElements.artist);
+
+        this.currentSong.title = songTitleEl.textContent;
+        this.currentSong.artist = songArtistEl.textContent;
+
+        if (!this.loadTextButton) {
+            this.renderButton();
+        }
+
+        this.loadTextButton.classList.remove('active');
+
+        let artist = this.currentSong.artist.toLowerCase().replace(/[^A-Za-z0-9]+/g, '').replace(/^the/, '');
+        let song = this.currentSong.title.toLowerCase().replace(/[^A-Za-z0-9]+/g, '');
+
+        chrome.runtime.sendMessage({
+            contentScriptQuery: 'fetchUrl',
+            url: 'http://www.azlyrics.com/lyrics/' + artist + '/' + song + '.html'
+        }, response => {
+            this.textPopup.innerHTML = '<div>' + response + '</div>';
+        });
+    },
+
+    onClick: function(event) {
+        if (!this.loadTextButton && !this.textPopup) {
+            return;
+        }
+
+        let eventEl = event.target || event.srcElement;
+        if (eventEl.id != this.loadTextButton.id) {
+            this.loadTextButton.classList.remove('active');
+            return;
+        }
+
+        this.loadTextButton.removeAttribute('title');
+        this.loadTextButton.classList.toggle('active');
+    },
+
+    renderButton: function() {
+        this.loadTextButton = document.createElement('div');
+        this.loadTextButton.id = 'load-text';
+        this.loadTextButton.className = 'load_text_btn';
+
+        this.playerControls[0].appendChild(this.loadTextButton);
+
+        this.textPopup = document.createElement('div');
+        this.textPopup.className = 'tooltiptext';
+
+        this.loadTextButton.appendChild(this.textPopup);
+    }
+}
+
+LyricsLoader.init();
